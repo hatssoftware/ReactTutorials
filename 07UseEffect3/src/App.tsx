5a2e6f83b3cb5ca6512840441878b4507edc4a14
steveruu <steveruu@stvr.cz>
2024-07-02 01:28:25 +0200

    Update App.tsx

--- 07UseEffect3/src/App.tsx
+++ 07UseEffect3/src/App.tsx
@@ -1,8 +1,17 @@
-import './App.css'
-
 let App = () => {
+  const url = "https://official-joke-api.appspot.com/random_joke";
+  const joke = "";
+
+  let getJoke = async () => {
+    const response = await fetch(url);
+    const data = await response.json();
+    console.log(data.setup);
+  }
+
+  getJoke();
+
   return (
-    <h1>nazdar</h1>
+    <h1>{joke}</h1>
   )
 };
 
