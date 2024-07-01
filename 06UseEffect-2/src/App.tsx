a4585b2774c77fe329e8225e0cc3a5f48cafe6fa
steveruu <steveruu@stvr.cz>
2024-07-02 00:43:12 +0200

    baaaaaaaaaaaaaaa

--- 06UseEffect-2/src/App.tsx
+++ 06UseEffect-2/src/App.tsx
@@ -1,11 +1,19 @@
 import { useState, useEffect } from "react";
 
 const App = () => {
-  
+  const [windowWidth, setWindowWidth] = useState(window.innerWidth);
+
+  useEffect(() => {
+    window.addEventListener("resize", () => {
+      setWindowWidth(window.innerWidth)
+    });
+
+  }, [window.innerWidth]) 
 
   return (
     <div>
-      <h1>Hello World</h1>
+      <h1>Šířka okna</h1>
+      <h2>{windowWidth} px</h2>
     </div>
   );
 }
