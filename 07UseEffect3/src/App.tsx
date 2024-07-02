048fcf71d8e7eee400ac7eb817e6508958372956
steveruu <steveruu@stvr.cz>
2024-07-02 13:20:44 +0200

    dostal jsem ratelimit takze jsem nahradil api

--- 07UseEffect3/src/App.tsx
+++ 07UseEffect3/src/App.tsx
@@ -1,17 +1,26 @@
-let App = () => {
-  const url = "https://official-joke-api.appspot.com/random_joke";
-  const joke = "";
+import { useState, useEffect } from "react";
+
+const App = () => {
+  const [joke, setJoke] = useState("Loading...");
+
+  const url = "https://official-joke-api.appspot.com/random_joke"; // max 100 za 15 minut
+  const url2 = "https://api.kanye.rest";
 
   let getJoke = async () => {
-    const response = await fetch(url);
+    const response = await fetch(url2);
     const data = await response.json();
-    console.log(data.setup);
+    setJoke(data.quote);
   }
 
-  getJoke();
+  useEffect(() => {
+    getJoke();
+  }, []) // spousti pouze jednou, a uplne na konci renderu
 
   return (
-    <h1>{joke}</h1>
+    <>
+      <h1>Citát</h1>
+      <h2>{joke}</h2>
+    </>
   )
 };
 
