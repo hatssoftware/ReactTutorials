b67f560f817a2f1eed1c2d6aeb696779697891ea
steveruu <steveruu@stvr.cz>
2024-07-03 12:47:38 +0200

    ISS final

--- 08ISS-API/src/App.tsx
+++ 08ISS-API/src/App.tsx
@@ -3,15 +3,17 @@ import {useState, useEffect} from "react";
 const App = () => {
   const url = "http://api.open-notify.org/iss-now.json"
   const [timestamp, setTimestamp] = useState<Date>(new Date(0));
-  const [coords, setCoords] = useState<Array<number>>([0, 0]);
+  const [coords, setCoords] = useState<Array<string>>(["Loading...", "Loading..."]);
+  const [mapsEmbed, setEmbed] = useState<string>("");
 
   const getCoords = async () => {
     const response = await fetch(url);
     const data = await response.json();
     
-    setCoords([data.iss_position.latitude, data.iss_position.longitude])
+    setCoords([data.iss_position.latitude, data.iss_position.longitude]);
+    setEmbed(`<iframe src="https://maps.google.com/maps?q=${data.iss_position.latitude},${data.iss_position.longitude}&t=k&z=3&ie=UTF8&iwloc=B&amp;output=embed" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>`);
 
-    const cas = new Date(data.timestamp * 1000)
+    const cas = new Date(data.timestamp * 1000);
     setTimestamp(cas);
   }
 
@@ -24,8 +26,8 @@ const App = () => {
       <h1>{ timestamp.toLocaleTimeString() }</h1>
       <h3>Zeměpisná šířka: {coords[0]}</h3>
       <h3>Zeměpisná délka: {coords[1]}</h3>
+      <div dangerouslySetInnerHTML={ {__html: mapsEmbed} }></div>
     </>
   )
 }
-
 export default App;
