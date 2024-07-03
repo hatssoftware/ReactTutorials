fbf29605ac2bf6e900afdf59244a2cbf01366049
st:prchal <steveruu@stvr.cz>
2024-07-03 18:35:24 +0200

    finished

--- 09MultipleReturns/src/App.tsx
+++ 09MultipleReturns/src/App.tsx
@@ -1,25 +1,35 @@
 import { useState, useEffect } from "react";
 
-const url = "https://cataas.com/cat";
+const url = "https://southparkquotes.onrender.com/v1/quotes";
 
 const App = () => {
   const [loading, setLoading] = useState(true);
+  const [fullQuote, setFullQuote] = useState(["", ""]);
 
   useEffect(() => {
     fetch(url)
-      .then((response) => {
-        console.log(response);
-      })
-      .then((data) => {
-        console.log(data);
-      })
+      .then((response) => response.json())
+      .then((data) => data[0])
+      .then((quote) => {
+        console.log(quote.quote);
+        console.log(quote.character);
+    
+        setFullQuote([quote.quote, quote.character]);
+        setLoading(false);
+    }) 
   }, [])
 
   if (loading) {
     return <h2>Načítání stránky...</h2>
-  } else {
-    return <h2>Obsah stránky</h2>
   }
+    
+  return (
+    <>
+      <h2>South Park Quotes with React</h2>
+      <p className="quote">"{fullQuote[0]}"</p>
+      <p className="character">- {fullQuote[1]}</p>
+    </>
+  )
 }
 
 export default App;
