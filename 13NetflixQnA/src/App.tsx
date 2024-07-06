919966c41faa046fffc1df0f9a82f3ec5419094e
steveruu <steveruu@stvr.cz>
2024-07-06 15:04:37 +0200

    trosku lepsi reseni

--- 13NetflixQnA/src/App.tsx
+++ 13NetflixQnA/src/App.tsx
@@ -1,12 +1,15 @@
 import Question from "./components/Question";
+import data from "./data/data";
 
 const App = () => {
   
   return (
     <div className="question-wrapper">
-      <Question question="Kolik stojí měsíční předplatné?" answer="Měsíční předplatné stojí 340,- Kč / měsíc."/>
-      <Question question="Jak mohu platit?" answer="Platit můžete bankovním převodem, kartou nebo hotově."/>
-      <Question question="Jak mohu zrušit předplatné?" answer="Předplatné můžete zrušit kdykoliv v nastavení vašeho účtu."/>
+    {
+      data.map((item) => {
+        return <Question key={item.id} {...item} />
+      })
+    }  
     </div>
   )
 }
---
+++ 13NetflixQnA/src/data/data.tsx
@@ -0,0 +1,29 @@
+let data = [
+    {
+        id: 1,
+        question: "Co je Netflix zač?",
+        answer: "Netflix je americká společnost, která se zabývá streamováním filmů a seriálů. Byla založena v roce 1997 v Kalifornii. Netflix je jedním z největších streamovacích služeb na světě."
+    },
+    {
+        id: 2,
+        question: "Kolik má Netflix uživatelů?",
+        answer: "Netflix má přes 200 milionů uživatelů po celém světě."
+    },
+    {
+        id: 3,
+        question: "Jaké má Netflix ceny?",
+        answer: "Netflix má tři cenové tarify: Základní, Standardní a Premium. Základní tarif stojí 7,99 €, Standardní 11,99 € a Premium 15,99 €."
+    },
+    {
+        id: 4,
+        question: "Kolik má Netflix filmů a seriálů?",
+        answer: "Netflix má přes 15 000 filmů a seriálů."
+    },
+    {
+        id: 5,
+        question: "Kolik má Netflix zaměstnanců?",
+        answer: "Netflix má přes 10 000 zaměstnanců."
+    }
+]
+
+export default data;
