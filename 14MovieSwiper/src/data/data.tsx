a90124d20e2848c4b2f220045e830fc5abfd8f76
st:prchal <steveruu@stvr.cz>
2024-07-06 19:09:45 +0200

    final

--- 14MovieSwiper/src/data/data.tsx
+++ 14MovieSwiper/src/data/data.tsx
@@ -3,6 +3,9 @@ import movie2 from "../images/movie2.png";
 import movie3 from "../images/movie3.png";
 import movie4 from "../images/movie4.png";
 import movie5 from "../images/movie5.png";
+import movie6 from "../images/movie6.png";
+import movie7 from "../images/movie7.png";
+import movie8 from "../images/movie8.png";
 
 let data = [
     {
@@ -35,6 +38,24 @@ let data = [
         title: "Telefon pana Harrigana",
         description: "Consectetur adipisicing elit. Lorem, ipsum dolor sit amet"
     },
+    {
+        id: 5,
+        image: movie6,
+        title: "Stážista",
+        description: "Adipisicing elit. Lorem, ipsum dolor sit amet consectetur"
+    },
+    {
+        id: 6,
+        image: movie7,
+        title: "Život",
+        description: "Dolor sit amet consectetur adipisicing elit. Lorem, ipsum"
+    },
+    {
+        id: 7,
+        image: movie8,
+        title: "Kód za miliardu dolarů",
+        description: "Sit amet consectetur adipisicing elit. Lorem, ipsum dolor"
+    }
 ]
 
 export default data;
